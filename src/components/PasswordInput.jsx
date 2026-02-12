c7f85ab9af7e79f2da22428681842e1776b4c967
Tryston <trystonproffit@gmail.com>
2026-02-12 14:02:38 -0500

    Finished Lesson

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,9 +1,16 @@
 // Code PasswordInput Component Here
 
 function PasswordInput (){
+    const handleChange = (event) => {
+        console.log("Entering password...");
+    };
+
     return(
-        <></>
-    )
+            <input
+            type="password"
+            onChange={handleChange}
+            />
+    );
 }
 
 export default PasswordInput;
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,8 +1,22 @@
 // Code SubmitButton Component Here
 
 function SubmitButton (){
+    const handleMouseEnter = () => {
+        console.log("Mouse Entering");
+    };
+
+    const handleMouseLeave = () => {
+        console.log("Mouse Exiting");
+    };
+
     return(
-        <></>
+        <button
+            type="submit"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+            Submit Password
+        </button>
     )
 }
 
